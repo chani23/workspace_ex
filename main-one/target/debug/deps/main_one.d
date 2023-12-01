0b0587034a22637b5a2285a89762010285737b80
Chani Kim <kimchani03@hanyang.ac.kr>
2023-12-01 09:56:14 +0900

    basic of workspace

---
+++ main-one/target/debug/deps/main_one.d
@@ -0,0 +1,5 @@
+C:\Users\82102\rust\workspace_ex\main-one\target\debug\deps\main_one.exe: src\main.rs
+
+C:\Users\82102\rust\workspace_ex\main-one\target\debug\deps\main_one.d: src\main.rs
+
+src\main.rs:
---
+++ main-one/target/debug/main-one.d
@@ -0,0 +1,1 @@
+C:\Users\82102\rust\workspace_ex\main-one\target\debug\main-one.exe: C:\Users\82102\rust\workspace_ex\main-one\src\main.rs
